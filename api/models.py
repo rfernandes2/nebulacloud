0f0c35a8c4491bafdeac5dbdbb3b59e451c212cb
João Moreira <j38moreira@gmail.com>
2024-11-14 20:54:31 +0000

    update models (path)

--- api/models.py
+++ api/models.py
@@ -6,6 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)  # Primary Key
     username = db.Column(db.String(80), unique=True, nullable=False)  # Unique Username
     password = db.Column(db.String(200), nullable=False)  # Password (hashed)
+    path = db.Column(db.String(50), nullable=False)
 
     def __repr__(self):
         return f"<User {self.username}>"
